3ffaa8d245a2bb887e322a898faec3ebb30a5fb3
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-08-30 11:55:10 +0100

    update the index.js file

--- data.js
+++
@@ -1,8 +0,0 @@
-const sessions = {
-    "id": "1",
-    "name": "John Cena",
-    "age": "32",
-    "sex": "male"
-}
-
-module.exports = sessions;
--- index.js
+++ index.js
@@ -2,39 +2,47 @@ const express = require('express');
 const fs = require('fs');
 const Debug = require('debug');
 const chalk = require('chalk');
-const http = require('http');
 
 const app = express();
 
 const dummyData = require('./data');
+// const studentRouter = express.Router();
 
-const debug = Debug('http');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 200,
-        message: 'Welcome to a simple node server',
-    });
+  res.status(200).json({
+    status: 200,
+    message: 'Welcome to a simple node server',
+  });
 })
 
 const data = JSON.stringify(dummyData);
-app.get('/write', (req, res) => {
+app
+  .get('/retrieve', (req, res) => {
+    let rawData = fs.readFile('./data.json', (error, data) => {
+      if(error) throw error 
+      let student = JSON.parse(data);
+      res.json(student);
+    });
+  })
+
+  .get('/add', (req, res) => {
     fs.writeFile('./fufu.json', data, error => {
-        if(!error) {
-            console.log('successful');
-        }
+      if (!error) {
+        console.log('successful');
+      }
     })
     res.status(200).json({
-        status: 200,
-        message: 'Json data added successfully'
+      status: 200,
+      message: 'Json data added successfully'
     })
-});
-
+  });
+// app.use('/api', studentRouter);
 
 app.listen(port, () => {
-    console.log(chalk.blue(`Server listening on port ${port}`));
+  console.log(chalk.blue(`Server listening on port ${port}`));
 })
